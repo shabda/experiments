ec1be3ddd7723f9f8c498b8bdf4aa9f64b5d0260
Shabda Raaj <shabda@agiliq.com>
2011-12-14 18:04:55 +0530

    Shortened a bit

--- q2.coffee
+++ q2.coffee
@@ -1,13 +1,10 @@
-[a, b] = [1, 2]
-fibs = [1, 2]
+[a, b] = fibs = [1, 2]
 
 loop
 	[a, b] = [b, a+b]
 	break if b > 4000000
 	fibs.push(b)
 even_fibs = (num for num in fibs when num % 2 == 0)	
-sum = 0
-for num in even_fibs
-	sum += num
+sum = even_fibs.reduce((x, y) -> x + y) 
 
 console?.log(sum)
--- q2.js
+++ q2.js
@@ -1,9 +1,7 @@
 (function() {
-  var a, b, even_fibs, fibs, num, sum, _i, _len, _ref, _ref2;
+  var a, b, even_fibs, fibs, num, sum, _ref, _ref2;
 
-  _ref = [1, 2], a = _ref[0], b = _ref[1];
-
-  fibs = [1, 2];
+  _ref = fibs = [1, 2], a = _ref[0], b = _ref[1];
 
   while (true) {
     _ref2 = [b, a + b], a = _ref2[0], b = _ref2[1];
@@ -21,12 +19,9 @@
     return _results;
   })();
 
-  sum = 0;
-
-  for (_i = 0, _len = even_fibs.length; _i < _len; _i++) {
-    num = even_fibs[_i];
-    sum += num;
-  }
+  sum = even_fibs.reduce(function(x, y) {
+    return x + y;
+  });
 
   if (typeof console !== "undefined" && console !== null) console.log(sum);
 
