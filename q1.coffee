53f51ffb3b8e04591fce167f7addc08b06231a5e
Shabda Raaj <shabda@agiliq.com>
2011-12-14 17:59:01 +0530

    Converted to a one liner

--- q1.coffee
+++ q1.coffee
@@ -1,5 +1,2 @@
-numbers = (num for num in [1..1000] when num % 3==0 or num % 5 ==0)
-sum = 0
-for num in numbers
-	sum += num
-console?.log sum
+console?.log  (num for num in [1..1000] when num % 3==0 or num % 5 ==0).reduce((x,y) -> x + y)
+
--- q1.js
+++ q1.js
@@ -1,22 +1,17 @@
 (function() {
-  var num, numbers, sum, _i, _len;
+  var num;
 
-  numbers = (function() {
-    var _results;
-    _results = [];
-    for (num = 1; num <= 1000; num++) {
-      if (num % 3 === 0 || num % 5 === 0) _results.push(num);
-    }
-    return _results;
-  })();
-
-  sum = 0;
-
-  for (_i = 0, _len = numbers.length; _i < _len; _i++) {
-    num = numbers[_i];
-    sum += num;
+  if (typeof console !== "undefined" && console !== null) {
+    console.log(((function() {
+      var _results;
+      _results = [];
+      for (num = 1; num <= 1000; num++) {
+        if (num % 3 === 0 || num % 5 === 0) _results.push(num);
+      }
+      return _results;
+    })()).reduce(function(x, y) {
+      return x + y;
+    }));
   }
-
-  if (typeof console !== "undefined" && console !== null) console.log(sum);
 
 }).call(this);
