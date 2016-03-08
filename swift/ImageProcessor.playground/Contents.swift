6b86958b45b06e5d5195f4751f12c757b8619c85
Shabda Raaj <shabda@agiliq.com>
2016-03-09 00:21:25 +0530

    Added ligten method

--- swift/ImageProcessor.playground/Contents.swift
+++ swift/ImageProcessor.playground/Contents.swift
@@ -50,6 +50,30 @@ func removeBlue(image: UIImage) -> UIImage{
     return removeChannel(image, channel: Channel.Blue)
 }
 
+func grayScale(image: UIImage) -> UIImage{
+    let transformer = {
+        (var pixel: Pixel) -> Pixel in
+        let avg = UInt8((Int(pixel.red) + Int(pixel.green) + Int(pixel.blue))/3)
+        (pixel.green, pixel.red, pixel.blue) = (avg, avg, avg)
+        return pixel
+    }
+    return transformImage(image, transformer: transformer)
+}
+
+func ligtenBy(image: UIImage, by: Int) ->UIImage{
+    let transformer = {
+        (var pixel: Pixel) -> Pixel in
+        let maxValue = 255
+        let green: Int = min(maxValue, Int(pixel.green)+by)
+        let red: Int = min(maxValue, Int(pixel.red)+by)
+        let blue: Int = min(maxValue, Int(pixel.blue)+by)
+        (pixel.green, pixel.red, pixel.blue) = (UInt8(green), UInt8(red), UInt8(blue))
+        return pixel
+    }
+    return transformImage(image, transformer: transformer)
+}
+
+ligtenBy(image!, by: 50)
 
 
 
